f2f5cc1a89c4b5bd7b79a822b78209711a232177
German <germanstarinenko@gmail.com>
2023-06-19 16:10:30 +0300

    add head

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
+from django.conf import settings
+from django.templatetags.static import static
 from django.urls import path
 
-from catalog.views import head, contacts
+from catalog.views import head
 
 urlpatterns = [
-     path("", contacts)
- ]
-
+     path("", head)
+ ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
